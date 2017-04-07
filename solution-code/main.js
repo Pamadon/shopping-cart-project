b3825803bbcc79504a80003f5a05ab34609bee19
Peter Amadon <peteramadon@Peters-MacBook-Pro.local>
2017-04-07 16:55:51 -0700

    stopping point

--- solution-code/main.js
+++ solution-code/main.js
@@ -1,1 +1,21 @@
+var juice = document.getElementById('juice')
+juice.addEventListener('click', function(event) {
+    document.getElementById('inner-flex1').classList.add('none2')
+    document.getElementById('inner-flex3').classList.add('none2')
+    document.getElementById('inner-flex2').classList.remove('none2')
+})
 
+var cereal = document.getElementById('cereal')
+cereal.addEventListener('click', function(event) {
+    document.getElementById('inner-flex2').classList.add('none2')
+    document.getElementById('inner-flex3').classList.add('none2')
+    document.getElementById('inner-flex1').classList.remove('none2')
+})
+
+
+var candy = document.getElementById('candy')
+candy.addEventListener('click', function(event) {
+    document.getElementById('inner-flex2').classList.add('none2')
+    document.getElementById('inner-flex1').classList.add('none2')
+    document.getElementById('inner-flex3').classList.remove('none2')
+})
